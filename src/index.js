65a176ee2a78ee1c98b0292020396450c8f1c042
John Daugherty <johndaugherty1986@gmail.com>
2017-09-22 20:05:54 -0500

    imported

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
